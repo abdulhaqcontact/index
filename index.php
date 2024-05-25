8cc954f745e2c36d5e036a3208b07faff4bdd810
Abdulhaq <abdulhaqcontact@gmail.com>
2024-05-25 00:58:25 -0700

    first commit

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+   <h1> how are you</h1>
+   
+</body>
+</html>
